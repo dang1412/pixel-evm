3b0c0a8984e1bdff6da79968b346f023e861eb2d
dang1412 <dttung1412@gmail.com>
2024-10-12 23:47:16 +0900

    hook useAdventures

--- src/components/Game/GameMap.tsx
+++ src/components/Game/GameMap.tsx
@@ -1,57 +1,23 @@
 'use client'
 
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { ViewportMap } from './ViewportMap'
-import { Adventures } from './adventures/Adventures'
+import { Adventures, ActionMode } from './adventures/Adventures'
+import { MonsterControl } from './Control'
+import { useAdventure } from './hooks/useAdventure'
 
 interface Props {}
 
-const MAP_H = 900
+const MAP_H = 800
 
 export const GameMap: React.FC<Props> = (props) => {
-  const canvasRef = useRef<HTMLCanvasElement>(null)
-
-  useEffect(() => {
-    const c = canvasRef.current
-    if (c) {
-      const vpmap = new ViewportMap(c)
-      // Create an instance of ResizeObserver
-      const resizeObserver = new ResizeObserver(entries => {
-        for (let entry of entries) {
-          // adjust the canvas size since we only watch canvas
-          const {width, height} = entry.contentRect
-          vpmap.resize(width, height)
-        }
-      })
-
-      resizeObserver.observe(c)
-
-      ;(async () => {
-        await vpmap.init()
-        const { width, height } = c.getBoundingClientRect()
-        vpmap.resize(width, height)
-        await vpmap.addImage('/images/pixel_logo.png', {x: 49, y: 49, w: 3, h: 3})
-        vpmap.moveCenter()
-
-        // Adventure game
-        const adventures = new Adventures(vpmap)
-        adventures.loadMonsters([
-          {id: 1, pos: 5055, hp: 10, type: 1},
-          {id: 2, pos: 5052, hp: 10, type: 2},
-        ])
-
-        adventures.startServer()
-      })()
-
-      return () => {
-        resizeObserver.disconnect()
-      }
-    }
-  }, [])
+  const [canvas, setCanvas] = useState<HTMLCanvasElement | null>(null)
+  const adventure = useAdventure(canvas)
 
   return (
     <>
-      <canvas ref={canvasRef} style={{border: '1px solid #ccc', width: '100%', height: MAP_H}} />
+      <canvas ref={(c) => setCanvas(c)} style={{border: '1px solid #ccc', width: '100%', height: MAP_H}} />
+      <MonsterControl onSetMode={(m) => {if (adventure) adventure.mode = m}} />
     </>
   )
 }
---
+++ src/components/Game/hooks/useAdventure.ts
@@ -0,0 +1,48 @@
+import { useEffect, useState } from 'react'
+import { Adventures } from '../adventures/Adventures'
+import { ViewportMap } from '../ViewportMap'
+
+export function useAdventure(c: HTMLCanvasElement | null): Adventures | undefined {
+  const [adventures, setAdventures] = useState<Adventures | undefined>()
+
+  useEffect(() => {
+    if (c) {
+      const vpmap = new ViewportMap(c)
+      // Create an instance of ResizeObserver
+      const resizeObserver = new ResizeObserver(entries => {
+        for (let entry of entries) {
+          // adjust the canvas size since we only watch canvas
+          const {width, height} = entry.contentRect
+          vpmap.resize(width, height)
+        }
+      })
+
+      resizeObserver.observe(c)
+
+      ;(async () => {
+        await vpmap.init()
+        const { width, height } = c.getBoundingClientRect()
+        vpmap.resize(width, height)
+        await vpmap.addImage('/images/pixel_logo.png', {x: 49, y: 49, w: 3, h: 3})
+        vpmap.moveCenter()
+
+        // Adventure game
+        const adventures = new Adventures(vpmap)
+        adventures.loadMonsters([
+          {id: 1, pos: 5055, hp: 10, type: 1},
+          {id: 2, pos: 5052, hp: 8, type: 2},
+        ])
+
+        adventures.startServer()
+
+        setAdventures(adventures)
+      })()
+
+      return () => {
+        resizeObserver.disconnect()
+      }
+    }
+  }, [c])
+
+  return adventures
+}
