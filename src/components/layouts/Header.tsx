f16442552c9727956872f09b81c835a2593600de
dang1412 <dttung1412@gmail.com>
2024-10-14 22:25:48 +0900

    feat: firebase hosting

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -14,17 +14,17 @@ export const Header = () => {
             </span>
           </a>
           <div className="flex items-center lg:order-2">
-            <a
+            {/* <a
               href="#"
               className="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800"
             >
               Log in
-            </a>
+            </a> */}
             <a
               href="#"
               className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2"
             >
-              Get started
+              Connect Wallet
             </a>
             {/* <button
               data-collapse-toggle="mobile-menu-2"
