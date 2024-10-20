7b7202dacba858ddc69b6aece67a4297bb64dfbb
dang1412 <dttung1412@gmail.com>
2024-10-21 02:12:02 +0900

    vconsole

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,19 @@ import dynamic from 'next/dynamic'
 import { Header } from '@/components/layouts/Header'
 import { Web3Provider } from '@/providers/Web3Provider'
 import TestConnect from '@/components/TestConnect/TestConnect'
+import { useEffect } from 'react'
 
 const GameMapLoad = () => import('@/components/Game/GameMap')
 const GameMap = dynamic(GameMapLoad, {ssr: false})
 
 export default function Page() {
+
+  useEffect(() => {
+    (async () => {
+      const { default: Vconsole } = await import('vconsole')
+      new Vconsole({})
+    })()
+  }, [])
 
   return (
     <Web3Provider>
