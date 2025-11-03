73252cdc06af7697c69be62a3017cd0bad2406ce
dang1412 <dttung1412@gmail.com>
2025-11-03 13:43:20 +0700

    feat(bomb): star explode animation

--- src/components/Game/Bomb/MapItem.ts
+++ src/components/Game/Bomb/MapItem.ts
@@ -2,8 +2,10 @@ import { Container, Graphics, Text } from 'pixi.js'
 import { BombMap } from './BombMap'
 import { PIXEL_SIZE } from '../utils'
 
+const flashAnimationTime = 500
+
 export class MapItem {
-  container = new Container()
+  private container = new Container()
 
   constructor(private bombMap: BombMap, private x: number, private y: number, private points: number) {
     const view = bombMap.map.getView()
@@ -11,7 +13,7 @@ export class MapItem {
     if (!mainScene) throw new Error('Main scene not loaded yet!')
 
     // Draw a star for the item
-    const star = new Graphics();
+    const star = new Graphics()
     star.star(
       PIXEL_SIZE / 2, // x
       PIXEL_SIZE / 2, // y
@@ -21,7 +23,7 @@ export class MapItem {
       0               // rotation
     )
     .fill(0xFFD700) // Gold color
-    .stroke({ width: 2, color: 0xFF4500 }); // Orange-Red outline
+    .stroke({ width: 2, color: 0xFF4500 }) // Orange-Red outline
 
     this.container.addChild(star)
 
@@ -44,6 +46,50 @@ export class MapItem {
   }
 
   remove() {
-    this.container.destroy()
+    // this.container.destroy()
+    this.explode()
+    const star = this.container.getChildAt(0) as Graphics
+    star.clear()
+  }
+
+  explode() {
+    const flash = new Graphics();
+    flash.circle(0, 0, PIXEL_SIZE / 3)
+      .fill(0xFF0000, 0.9);
+    flash.pivot.set(0, 0);
+    flash.x = PIXEL_SIZE / 2;
+    flash.y = PIXEL_SIZE / 2;
+    flash.scale.set(1);
+    this.container.addChild(flash);
+
+    const view = this.bombMap.map.getView()
+    const unsub = view.subscribe('tick', (e: CustomEvent<number>) => {
+      const delta = Math.min(e.detail, 40)
+      animateFlash(delta)
+    })
+
+    let flashElapsedTime = 0;
+
+    const text = this.container.getChildAt(1) as Text
+
+    const animateFlash = (delta: number) => {
+      flashElapsedTime += delta
+      let progress = flashElapsedTime / flashAnimationTime
+      if (progress > 1) progress = 1
+
+      const easeOut = 1 - Math.pow(1 - progress, 3)
+      
+      flash.scale.set(1 + easeOut * 2.5)
+      flash.alpha = 1 - easeOut
+
+      text.scale.set(0.2 + easeOut * 0.4)
+      text.alpha = 1.2 - easeOut
+
+      if (progress === 1) {
+        // app.ticker.remove(animateFlash)
+        unsub()
+        this.container.destroy()
+      }
+    }
   }
 }
