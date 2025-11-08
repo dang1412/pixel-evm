bad375892bc545fe6ac98020515b474f2eaccdb7
dang1412 <dttung1412@gmail.com>
2025-11-08 07:43:00 +0700

    update(bomb): guide content

--- src/components/Game/Bomb/GuideSteps.tsx
+++ src/components/Game/Bomb/GuideSteps.tsx
@@ -2,12 +2,12 @@ export const GuideSteps = [
   {
     content: (
       <div>
-        <p className="mt-2">This is a simple multi-players Bomb game.</p>
+        <p className="mt-2">This is a simple <b>real-time</b>, <b>multi-players</b> Bomb game.</p>
         <p className="mt-2">Devices can connect and play together—ideally on the same Wi-Fi or local network (via WebRTC).</p>
         <p className="ml-2">✅ One device acts as the host, and others connect to it.</p>
         <p className="ml-2">✅ P2p connection, no server required.</p>
         <p className="ml-2">✅ Instant connect. No account/signup required.</p>
-        <p className="mt-2">First, make the host by clicking on "Create game".</p>
+        <p className="mt-2">First, make the host by clicking on "<b>Create game</b>".</p>
       </div>
     ),
     image: "/bomb/guide/bomb-guide-1.png"
@@ -15,9 +15,9 @@ export const GuideSteps = [
   {
     content: (
       <div>
-        <p className="mt-2">Remember the host connect name, next to Scoreboard (8a5xwesa).</p>
         <p className="mt-2">Dashboard display no players.</p>
-        <p className="mt-2">Click "Join game" to be the first player.</p>
+        <p className="mt-2">Remember the host connect name, next to Scoreboard (<b>8a5xwesa</b>).</p>
+        <p className="mt-2">Click "<b>Join game</b>" to be the first player.</p>
       </div>
     ),
     image: "/bomb/guide/bomb-guide-2.png"
@@ -26,8 +26,8 @@ export const GuideSteps = [
     content: (
       <div>
         <h2 className="text-xl font-semibold">Single-player</h2>
-        <p className="mt-2">Confirm dashboard show the first player</p>
-        <p className="mt-2">Click "Start" to play in single mode</p>
+        <p className="mt-2">Confirm dashboard show the first player.</p>
+        <p className="mt-2">Click "<b>Start</b>" to play in single mode.</p>
       </div>
     ),
     image: "/bomb/guide/bomb-guide-3.png"
@@ -37,7 +37,7 @@ export const GuideSteps = [
       <div>
         <h2 className="text-xl font-semibold">Single-player</h2>
         <p className="mt-2">Choose bomb type, checking time countdown and score in the corner.</p>
-        <p className="mt-2">Click on map to place bomb, try to be near stars to blow them up.</p>
+        <p className="mt-2">Click on the map to place a bomb, try to be near stars to blow them up.</p>
       </div>
     ),
     image: "/bomb/guide/bomb-guide-4.png"
@@ -46,9 +46,9 @@ export const GuideSteps = [
     content: (
       <div>
         <h2 className="text-xl font-semibold">Multi-players</h2>
-        <p className="mt-2">On other device, click "Join a game".</p>
+        <p className="mt-2">On other device, at step 1 click "<b>Join a game</b>".</p>
         <p className="mt-2">Enter first device (host)'s connect name.</p>
-        <p className="mt-2">Click "Connect".</p>
+        <p className="mt-2">Click "<b>Connect</b>".</p>
       </div>
     ),
     image: "/bomb/guide/bomb-guide-5.png"
@@ -66,9 +66,9 @@ export const GuideSteps = [
     content: (
       <div>
         <h2 className="text-xl font-semibold">Multi-players</h2>
-        <p className="mt-2">Confirm the "Connected" notification.</p>
-        <p className="mt-2">Confirm the first player in the dashboard.</p>
-        <p className="mt-2">Click "Join game" to be the second player.</p>
+        <p className="mt-2">Confirm the "Connected to..." notification on both devices.</p>
+        <p className="mt-2">Confirm the first player on the dashboard.</p>
+        <p className="mt-2">Click "<b>Join game</b>" to be the second player.</p>
       </div>
     ),
     image: "/bomb/guide/bomb-guide-6.png"
@@ -88,7 +88,7 @@ export const GuideSteps = [
       <div>
         <h2 className="text-xl font-semibold">Multi-players</h2>
         <p className="mt-2">Confirm 2 players in the host's dashboard too.</p>
-        <p className="mt-2">Click "Start/Next" button to start the game.</p>
+        <p className="mt-2">Click "<b>Start/Next</b>" button to start the game.</p>
       </div>
     ),
     image: "/bomb/guide/bomb-guide-8.png"
